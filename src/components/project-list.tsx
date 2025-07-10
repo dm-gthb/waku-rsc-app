67ebfe868dcccb62468bc542d0f112bdec981acc
Dmitrii Mamonov <dm.ggl2022@gmail.com>
2025-07-10 12:14:14 +0400

    add link for create-project route

--- src/components/project-list.tsx
+++ src/components/project-list.tsx
@@ -8,31 +8,39 @@ type ProjectWithProgress = Project & {
 
 export function ProjectList({ projects }: { projects: Array<ProjectWithProgress> }) {
   return (
-    <div>
-      <div className="flex border-b border-gray-200 -mx-8">
-        {['Name', 'Priority', 'Target Date', 'Status'].map((title) => (
-          <Line key={title}>{title}</Line>
-        ))}
-      </div>
+    <>
+      <div className="mb-6">
+        <div className="flex border-b border-gray-200 -mx-8">
+          {['Name', 'Priority', 'Target Date', 'Status'].map((title) => (
+            <Line key={title}>{title}</Line>
+          ))}
+        </div>
 
-      <div className="flex flex-col -mx-8">
-        {projects.map(({ id, title, priority, targetDate, progress }) => (
-          <Link key={id} to={`/project/${id}/tasks`} className="flex hover:bg-gray-50 ">
-            <Line>{title}</Line>
-            <Line>{priority}</Line>
-            <Line>
-              {targetDate
-                ? new Intl.DateTimeFormat('en-US', {
-                    month: 'short',
-                    day: 'numeric',
-                  }).format(new Date(targetDate))
-                : ''}
-            </Line>
-            <Line>{progress !== null ? `${progress}%` : 'N/A'}</Line>
-          </Link>
-        ))}
+        <div className="flex flex-col -mx-8">
+          {projects.map(({ id, title, priority, targetDate, progress }) => (
+            <Link key={id} to={`/project/${id}/tasks`} className="flex hover:bg-gray-50 ">
+              <Line>{title}</Line>
+              <Line>{priority}</Line>
+              <Line>
+                {targetDate
+                  ? new Intl.DateTimeFormat('en-US', {
+                      month: 'short',
+                      day: 'numeric',
+                    }).format(new Date(targetDate))
+                  : ''}
+              </Line>
+              <Line>{progress !== null ? `${progress}%` : 'N/A'}</Line>
+            </Link>
+          ))}
+        </div>
       </div>
-    </div>
+      <Link
+        to="/create-project"
+        className="cursor-pointer min-w-24 bg-gray-500 hover:bg-gray-600 text-white font-bold py-2 px-4 rounded"
+      >
+        + Add Project
+      </Link>
+    </>
   );
 }
 
